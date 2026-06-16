fd680b3ed484c284f39a5716a633e4aca982a038
Avi Saini <sainiparvenu0@gmail.com>
2026-06-16 17:06:51 +0530

    initial commit

---
+++ Preplap_Assignment_1.py
@@ -0,0 +1,20 @@
+#Write a program to print name, age, course name and training ..!
+
+#Take~Input all the details from the students.
+
+name = input("Enter the student name: ")
+age = int(input("Enter student's age: "))
+course_name = input("Enter your current(pursuing) course: ")
+training_batch = "15-june-2026 to 30-07-2026 (6-Weeks)."
+
+#Print the details of the students..!
+
+print()
+print('*'*60)
+print("                    Student Details                   ")
+print("*"*60)
+print("Name is: ",name)
+print("Age is: ",age)
+print("CourseName is: ",course_name)
+print("Training Batch is: ",training_batch)
+print('*'*60)
